9fa6d6308811ea28a99acc918bce9582555a7a69
Sahil Kumar Jha <112754860+striversahil@users.noreply.github.com>
2024-10-20 19:05:59 +0530

    comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+// Commenting
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
